05c55dd2deb7c95b9772e2e37084be5cfba12ae8
Jefferson dos Anjos <0rakul0render@gmail.com>
2022-01-26 11:23:29 -0300

    att

--- js/api.js
+++ js/api.js
@@ -2,14 +2,17 @@ $("document").ready(function () {
     $("#btn_submit").click(function () {
         var peso = parseFloat($("#inp_peso").val());
         var altura = parseFloat($("#inp_altura").val());
-        $.ajax({
+
+        $ajax({
             url: "http://0rakul0.pythonanywhere.com/api",
-            type: "POST",
+            method: "POST",
             contentType: "application/json",
             data: JSON.stringify({
                 "peso": peso,
                 "altura": altura
+            }).done(function (data) {
+                $("h1").html("IMC: " + data["imc"]);
             })
-        }).done(function (data) { $("h1").html("IMC: " + data["resultado"]) })
+        })
     });
 });
